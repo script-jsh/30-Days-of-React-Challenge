d95c2f66dc500bc12641adea4d29df5afb92fbcb
script-jsh <codescript19@gmail.com>
2022-09-15 16:42:26 +0000

    Footer class component added

--- day-07/class-component/src/App.js
+++ day-07/class-component/src/App.js
@@ -1,11 +1,13 @@
 import Header from "./component/Header";
 import Main from "./component/Main";
+import Footer from "./component/Footer";
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
---
+++ day-07/class-component/src/component/Footer.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export class Footer extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+    return (
+      <footer>
+        <div className="footer-wrapper">
+            <p>Copyright 2022</p>
+        </div>
+      </footer>
+    )
+  }
+}
+
+export default Footer
