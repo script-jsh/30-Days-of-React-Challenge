97d8cdf5a85df6c730a620aa3636ae28c27b15c5
script-jsh <codescript19@gmail.com>
2022-09-15 14:52:14 +0000

    Header component added

--- day-07/class-component/src/component/Header.jsx
+++ day-07/class-component/src/component/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
-function Header() {
-  return (
-    <div>Header</div>
-  )
+// Class based component
+class Header extends React.Component {
+    constructor(props) {
+        super(props)
+        // The code inside the constructor runs before any other code
+    }
+    render() {
+        return (
+            <header>
+                <div className="header_wrapper">
+                    <h1>Welcome to Day 07 - Class Component</h1>
+                    <h2>Getting Started with React</h2>
+                    <h3>JavaScript Library</h3>
+                    <p>Alhassan Joshua</p>
+                    <small>15th September, 2022</small>
+                </div>
+            </header>
+        )
+    }
 }
 
 export default Header
